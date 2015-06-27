088a0d472a5f20f06fec1d186a5629586a16fc18
Peter Howell <peter.e.howell@gmail.com>
2015-06-26 21:31:08 -0230

    Added a temp image so we have something to work with.

---
+++ Vitante/Sprite.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Vitante {
+    class Sprite {
+        Texture texture;
+        Vector2 position;
+        public Sprite(Texture texture) {
+
+        }
+    }
+}
